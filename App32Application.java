5438543e6558ef75962de9d6e8d4e247314760bc
msrph9248 <msrph9248@gmail.com>
2020-04-07 19:41:47 -0700

    Added App32Application and GitHubUser java files

---
+++ App32Application.java
@@ -0,0 +1,13 @@
+package com.srini.App32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App32Application.class, args);
+	}
+
+}
